cae9c698b58159cc5dfd389e6c2234b14ac41e04
yafet188 <tegbaruyafet18@gmail.com>
2025-03-17 23:18:33 -0400

    Added Connect With Our Fathers Form section.

--- app/ContactUs/page.tsx
+++ app/ContactUs/page.tsx
@@ -1,10 +1,17 @@
 // app/pages/ContactUs.tsx
 import React from "react";
 import Image from "next/image";
-import { Urbanist, Outfit } from "next/font/google";
+import { Urbanist, Outfit, Raleway } from "next/font/google";
 
-const urbanist = Urbanist({ subsets: ["latin"], weight: ["400"] });
+const urbanist = Urbanist({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
 const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+const raleway = Raleway({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
 });
@@ -112,6 +119,119 @@ const ContactUs = () => {
           >
             BOOK A CONFESSION
           </button>
+        </div>
+      </div>
+
+      {/* Connect With Our Fathers Form*/}
+      <div className="w-full h-[1006px] bg-[#B7B9C0] py-[100px] flex justify-center">
+        {/* Outer Container for Text & Form */}
+        <div className="w-[1285.96px] h-[806px] pt-[32px] pb-[32px] gap-[32px] flex justify-between items-center">
+          {/* Left Side - Text Container */}
+          <div className="w-[626.98px] h-[148px] gap-[32px] flex flex-col">
+            {/* Placeholder for heading and description */}
+            <h3
+              className={`${urbanist.className} w-[667px] h-[62px] font-[700] text-[52px] leading-[120%] tracking-[-0.02em] text-[#171E34] `}
+            >
+              Connect With Our Fathers
+            </h3>
+
+            <p
+              className={`${urbanist.className} w-[447.61px] h-[54px] font-[700] text-[18px] leading-[150%] tracking-[0.02rem] text-[#454B5D] `}
+            >
+              Please use this form to schedule a confession, request a prayer,
+              or submit inquiries.
+            </p>
+          </div>
+
+          {/* Right Side - Form Container */}
+          <div className="w-[626.98px] h-[760px] bg-[#0A0D16] text-white p-[32px] gap-[48px] rounded-[24px] border border-[#151B2F] shadow-lg">
+            {/* Form Placeholder */}
+            <div className="w-[562.98px] h-[585px] flex flex-col gap-[24px]">
+              {/* Name Section */}
+              <div className="flex flex-col gap-[8px]">
+                <label
+                  className={`${outfit.className} text-[14px] font-[600] leading-[150%] tracking-[0.02em] text-white`}
+                >
+                  NAME
+                </label>
+
+                <input
+                  type="text"
+                  placeholder="First & Last Name"
+                  className={`${raleway.className} w-[562.98px] h-[56px] rounded-[16px] p-[14px] bg-[#151B2F] text-[#9498A2] font-[400] text-[14px] leading-[120%] tracking-[0.02em] focus:outline-none focus:ring-2 focus:ring-[#E5A93D]`}
+                />
+              </div>
+
+              {/* Email Address Section} */}
+              <div className="flex flex-col gap-[8px]">
+                <label
+                  className={`${outfit.className} text-[14px] font-[600] leading-[150%] tracking-[0.02em] text-white`}
+                >
+                  EMAIL ADDRESS
+                </label>
+
+                <input
+                  type="text"
+                  placeholder="email@provider.com"
+                  className={`${raleway.className} w-[562.98px] h-[56px] rounded-[16px] p-[14px] bg-[#151B2F] text-[#9498A2] font-[400] text-[14px] leading-[120%] tracking-[0.02em] focus:outline-none focus:ring-2 focus:ring-[#E5A93D]`}
+                />
+              </div>
+
+              {/* Select A Father Section */}
+              <div className="flex flex-col gap-[8px]">
+                <label
+                  className={`${outfit.className} text-[14px] font-[600] leading-[150%] tracking-[0.02em] text-white`}
+                >
+                  SELECT A FATHER
+                </label>
+
+                <input
+                  type="text"
+                  placeholder="Select Father"
+                  className={`${raleway.className} w-[562.98px] h-[56px] rounded-[16px] p-[14px] bg-[#151B2F] text-[#9498A2] font-[400] text-[14px] leading-[120%] tracking-[0.02em] focus:outline-none focus:ring-2 focus:ring-[#E5A93D]`}
+                />
+              </div>
+
+              {/* Subject Section */}
+              <div className="flex flex-col gap-[8px]">
+                <label
+                  className={`${outfit.className} text-[14px] font-[600] leading-[150%] tracking-[0.02em] text-white`}
+                >
+                  SUBJECT
+                </label>
+
+                <input
+                  type="text"
+                  placeholder="ex. Confession"
+                  className={`${raleway.className} w-[562.98px] h-[56px] rounded-[16px] p-[14px] bg-[#151B2F] text-[#9498A2] font-[400] text-[14px] leading-[120%] tracking-[0.02em] focus:outline-none focus:ring-2 focus:ring-[#E5A93D]`}
+                />
+              </div>
+
+              {/* Message Section */}
+              <div className="flex flex-col gap-[8px]">
+                <label
+                  className={`${outfit.className} text-[14px] font-[600] leading-[150%] tracking-[0.02em] text-white`}
+                >
+                  MESSAGE
+                </label>
+
+                <textarea
+                  placeholder="What is your inquiry..."
+                  className={`${raleway.className} w-[562.98px] h-[120px] rounded-[16px] p-[14px] bg-[#151B2F] text-[#9498A2] font-[400] text-[14px] leading-[120%] tracking-[0.02em] focus:outline-none focus:ring-2 focus:ring-[#E5A93D]`}
+                />
+              </div>
+
+              {/*Submit Button*/}
+              <div className="flex flex-col gap-[48px]">
+                <button
+                  type="submit"
+                  className={`${outfit.className} w-[562.98px] h-[48px] rounded-[8px] px-[12px] py-[16px] gap-[6px] text-[14px] leading-[150%] tracking-[0.02em] bg-[#E0AE54] text-white`}
+                >
+                  SUBMIT
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
