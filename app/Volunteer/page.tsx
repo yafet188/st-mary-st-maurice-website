3539ceae026a70d6390e71fff8e9cc2c6a427a97
Fady <fadyg06@gmail.com>
2025-04-07 15:11:53 -0400

    Volunteer page and ImageTextBlock component

--- app/Volunteer/page.tsx
+++ app/Volunteer/page.tsx
@@ -1,3 +1,53 @@
+import ImageTextBlock from "../components/ImageTextBlock";
+import OrdainingDeaconsPicture from "../../public/Images/Volunteer/OrdainingDeacons.jpg";
+import DeaconUnderSunPicture from "../../public/Images/Volunteer/DeaconUnderSun.jpg";
+import AltarDuringLiturgy from "../../public/Images/Volunteer/AltarDuringLiturgy.jpg";
+
 export default function Volunteer() {
-  return <div>{/* Content here */}</div>;
+  // note to self: fix the apostrophe errors?
+  // ALSO NOTE btnHoverColor is NOT yet implemented
+  return (
+    <div>
+      <ImageTextBlock
+        inverted={false}
+        title="Serve, Support, Succeed: Volunteer Today"
+        image={OrdainingDeaconsPicture}
+        altText="A picture of the priests and Bishop Mina with the newly ordained young chanters in St. Maurice Church"
+        bgColor="#FEFAF1"
+      >
+        At St. Mary and St. Maurice's, volunteering is about making a meaningful
+        difference. You can engage in our missions through community outreach,
+        educational programs, or maintaining our worship services.
+      </ImageTextBlock>
+      <ImageTextBlock
+        inverted={true}
+        title="Give Your Time, Transform Lives"
+        image={DeaconUnderSunPicture}
+        altText="A picture of a deacon under ray of sunlight in St.Maurice's Church"
+        bgColor="#E8E9EB"
+        btnTxt="SERVE WITH US"
+        btnColor="#171E34"
+        btnHoverColor="#7A0C02"
+      >
+        Each volunteer role supports our church's activities and offers personal
+        growth. Join our dedicated team of volunteers to contribute to a network
+        of care that extends throughout Kitchener, strengthening both the church
+        and its wider community.
+      </ImageTextBlock>
+      <ImageTextBlock
+        inverted={false}
+        title="Join Our Mission: Volunteer with Us"
+        image={AltarDuringLiturgy}
+        altText="A picture of the back of Fr. Theodore in the foreground and the deacons holding candles around the altar with Fr.Athanasius in the background"
+        bgColor="#FEFAF1"
+        miniTitle="Your Efforts Are Crucial"
+        btnTxt="GET INVOLVED"
+        btnColor="#7A0C02"
+        btnHoverColor="#171E34"
+      >
+        Your efforts embody our mission to spread love and foster spiritual
+        growth, creating lasting impacts and deeper community connections.
+      </ImageTextBlock>
+    </div>
+  );
 }
---
+++ app/components/ImageTextBlock.tsx
@@ -0,0 +1,109 @@
+import { ReactNode } from "react";
+import Image from "next/image";
+import type { StaticImageData } from "next/image";
+
+interface ImageTextBlockProps {
+  children: ReactNode;
+  image: StaticImageData;
+  inverted: boolean;
+  title: string;
+  altText: string;
+  bgColor: "#FEFAF1" | "#E8E9EB";
+  miniTitle?: string;
+  btnTxt?: string;
+  btnColor: string;
+  btnHoverColor?: string;
+  btnLink?: string;
+}
+
+interface RoundedImageProps {
+  image: StaticImageData;
+  altText: string;
+}
+
+interface RoundedButtonProps {
+  children: string;
+  color: string;
+  hovercolor?: string;
+  link?: string;
+}
+
+// Special component specifically to deal with image styling, reassess if necessary
+const RoundedImage = ({ image, altText }: RoundedImageProps) => {
+  return (
+    <Image
+      className="w-[616px] h-[392.69px] rounded-[24px] object-cover"
+      src={image}
+      alt={altText}
+    ></Image>
+  );
+};
+
+// ditto for buttons
+const RoundedButton = ({ children, color, link }: RoundedButtonProps) => {
+  const btnStyle = `w-[142px] h-[45px]  text-white font-[600] text-[14px] leading-[120%] tracking-[0.02em] rounded-[8px] flex items-center justify-center transition duration-300 ease-out bg-[${color}]`;
+
+  if (link != "") {
+    return (
+      <a href={link}>
+        <button className={btnStyle}>{children}</button>
+      </a>
+    );
+  } else {
+    return <button className={btnStyle}>{children}</button>;
+  }
+};
+
+const ImageTextBlock = ({
+  children,
+  image,
+  inverted,
+  title,
+  miniTitle,
+  altText,
+  bgColor,
+  btnTxt,
+  btnColor,
+  btnLink,
+  btnHoverColor,
+}: ImageTextBlockProps) => {
+  return (
+    <div className={`w-full bg-[${bgColor}]`}>
+      <div
+        className={`bg-[${bgColor}] p-[100px] flex flex-row items-center gap-[80px] w-[1512px] mx-auto`}
+      >
+        {!inverted && (
+          <RoundedImage image={image} altText={altText}></RoundedImage>
+        )}
+        <div className="w-[616px] flex flex-col gap-[24px]">
+          <h2 className="font-[outfit] font-bold text-[36px] leading-[120%] tracking-[2%] uppercase">
+            {title}
+          </h2>
+          {miniTitle && (
+            <h6 className="font-[outfit] font-bold text-[20px] leading-[120%] tracking-[2%]">
+              Your Efforts Are Crucial
+            </h6>
+          )}
+          <div className="font-[urbanist] font-normal text-[18px] leading-[150%] tracking-[2%] text-[#151B2F]">
+            {children}
+          </div>
+          {btnTxt && (
+            <RoundedButton
+              link={btnLink}
+              hovercolor={btnHoverColor}
+              color={btnColor}
+            >
+              {btnTxt}
+            </RoundedButton>
+          )}
+        </div>
+
+        {inverted && (
+          <RoundedImage image={image} altText={altText}></RoundedImage>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ImageTextBlock;
